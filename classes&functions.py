06a3d5525419da70d4150aec5d0ad545b880d35d
HalaKorhani <halakorhani2005@icloud.com>
2025-02-27 21:34:05 +0200

    adding the constructor with the the attributes needed

--- classes&functions.py
+++ classes&functions.py
@@ -1,2 +1,7 @@
 class Vehicle:
     
+    def __init__(self, brand, model, year, _rental_price_per_day):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self._rental_price_per_day = _rental_price_per_day
