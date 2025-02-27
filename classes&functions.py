1d9177a6cde54290a857ffa09f0a773e5d23c37a
HalaKorhani <halakorhani2005@icloud.com>
2025-02-27 21:55:54 +0200

    Add display_info method to Vehicle class to display vehicle details

--- classes&functions.py
+++ classes&functions.py
@@ -1,1 +1,9 @@
 class Vehicle:
+    def __init__(self, brand, model, year, _rental_price_per_day):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self._rental_price_per_day = _rental_price_per_day
+    def display_info(self):
+        return f"Vehicle: {self.brand} {self.model}, Year: {self.year}, Rental Price: ${self._rental_price_per_day}/day"
+
