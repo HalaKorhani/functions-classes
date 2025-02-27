5ab2f583d64d4ebf6837b979e500385bac788bc1
HalaKorhani <halakorhani2005@icloud.com>
2025-02-27 21:42:20 +0200

    create a class named vehicle

--- classes&functions.py
+++ classes&functions.py
@@ -1,7 +1,1 @@
 class Vehicle:
-    
-    def __init__(self, brand, model, year, _rental_price_per_day):
-        self.brand = brand
-        self.model = model
-        self.year = year
-        self._rental_price_per_day = _rental_price_per_day
