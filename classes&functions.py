90328d88a46ee14f4c54850ace2b44338f0c8eff
HalaKorhani <halakorhani2005@icloud.com>
2025-02-28 21:41:56 +0200

    deleting (calculate-rental cost)

--- classes&functions.py
+++ classes&functions.py
@@ -6,9 +6,7 @@ class Vehicle:
         self._rental_price_per_day = _rental_price_per_day
     def display_info(self):
         return f"Vehicle: {self.brand} {self.model}, Year: {self.year}, Rental Price: ${self._rental_price_per_day}/day"
-    def calculate_rental_cost(self, days):
-        return self._rental_price_per_day * days
-
+    
 class Car(Vehicle):
     def __init__(self, brand, model, year, _rental_price_per_day, seating_capacity):
         super().__init__(brand, model, year, _rental_price_per_day)
